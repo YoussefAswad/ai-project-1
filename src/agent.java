7f3f763954912753e63f9a97b0e212478504df83
mostafamohamedaly <mostafama9095@gmail.com>
2022-12-04 23:34:20 +0200

    Update agent.java

--- src/agent.java
+++ src/agent.java
@@ -12,4 +12,28 @@ public class agent{
         this.capacity = rand.nextInt(30, 101);
     }
 
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
 }
